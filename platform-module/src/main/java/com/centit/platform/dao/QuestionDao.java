03b6ccc41044246d9582e0cceb7ab67be9abd28d
zhou_c <zhou_c@centit.com>
2021-09-15 13:57:06 +0800

    抽取出项目组api独立一个模块

--- platform-module/src/main/java/com/centit/platform/dao/QuestionDao.java
+++
@@ -1,9 +0,0 @@
-package com.centit.platform.dao;
-
-import com.centit.framework.jdbc.dao.BaseDaoImpl;
-import com.centit.platform.po.Question;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class QuestionDao extends BaseDaoImpl<Question,String> {
-}
