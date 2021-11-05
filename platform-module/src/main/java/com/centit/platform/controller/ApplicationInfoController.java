4345f9f7af586a495f8c19636ed4196f093b2dac
zhou_c <zhou_c@centit.com>
2021-11-05 11:13:48 +0800

    修改接口

--- platform-module/src/main/java/com/centit/platform/controller/ApplicationInfoController.java
+++ platform-module/src/main/java/com/centit/platform/controller/ApplicationInfoController.java
@@ -1,12 +1,20 @@
 package com.centit.platform.controller;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.centit.framework.common.WebOptUtils;
 import com.centit.framework.core.controller.BaseController;
 import com.centit.framework.core.controller.WrapUpResponseBody;
+import com.centit.framework.model.adapter.PlatformEnvironment;
+import com.centit.framework.model.basedata.IOptMethod;
 import com.centit.framework.model.basedata.IOsInfo;
+import com.centit.framework.system.po.OptMethod;
 import com.centit.framework.system.po.OsInfo;
+import com.centit.metaform.service.MetaFormModelDraftManager;
+import com.centit.metaform.service.MetaFormModelManager;
 import com.centit.platform.service.ApplicationInfoManager;
+import com.centit.support.database.utils.PageDesc;
+import com.centit.workflow.service.FlowOptService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -16,8 +24,10 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author zhf
@@ -28,6 +38,18 @@ import java.util.Map;
 public class ApplicationInfoController extends BaseController {
     @Autowired
     private ApplicationInfoManager applicationInfoManager;
+
+    @Autowired
+    private MetaFormModelManager metaFormModelManager;
+
+    @Autowired
+    private MetaFormModelDraftManager metaFormModelDraftManager;
+
+    @Autowired
+    private PlatformEnvironment platformEnvironment;
+
+    @Autowired(required = false)
+    FlowOptService flowOptService;
 
     @ApiOperation(value = "新增应用")
     @PostMapping
@@ -68,5 +90,41 @@ public class ApplicationInfoController extends BaseController {
     @WrapUpResponseBody
     public JSONObject getApplicationInfo(@PathVariable String applicationId) {
         return applicationInfoManager.getApplicationInfo(applicationId);
+    }
+
+    @ApiOperation(value = "业务模块删除按钮")
+    @PostMapping(value = "/businessDelete")
+    @WrapUpResponseBody
+    public JSONObject businessDelete(String optId,  String topUnit,String applicationId) {
+        JSONObject jsonObject = new JSONObject();
+        PageDesc pageDesc = new PageDesc();
+        pageDesc.setPageNo(1);
+        pageDesc.setPageSize(10);
+        Map<String, Object> metaFormParam = new HashMap<>();
+        metaFormParam.put("optId",optId);
+        //页面数据
+      /*  JSONArray metaFormModelList = metaFormModelManager.listFormModeAsJson(null, metaFormParam, pageDesc);
+        JSONArray metaFormModelDraftList = metaFormModelDraftManager.listFormModeAsJson(null, metaFormParam, pageDesc);
+        if (!metaFormModelList.isEmpty() || !metaFormModelDraftList.isEmpty()){
+            jsonObject.put("msg","页面存在数据，无法删除，请先移除！");
+            return jsonObject;
+        }*/
+        //接口数据
+        List<? extends IOptMethod> iOptMethods = platformEnvironment.listAllOptMethod(topUnit);
+        List<String> optIds = iOptMethods.stream().map(iOptMethod -> iOptMethod.getOptId()).collect(Collectors.toList());
+        if (optIds.contains(optId)){
+            jsonObject.put("msg","接口存在数据，无法删除，请先移除！");
+            return jsonObject;
+        }
+        //流程数据
+        Map<String, Object> map = new HashMap<>();
+        map.put("applicationId",applicationId);
+        map.put("optId",optId);
+        JSONArray flowOptInfoList = flowOptService.listOptInfo(map, pageDesc);
+        if (!flowOptInfoList.isEmpty()){
+            jsonObject.put("msg","流程存在数据，无法删除，请先移除！");
+            return jsonObject;
+        }
+        return jsonObject;
     }
 }
