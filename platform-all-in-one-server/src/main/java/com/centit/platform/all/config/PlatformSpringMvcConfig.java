56f22a70eb3a8ea7ff5147b4b5ae73782620a7f2
zhou_c <zhou_c@centit.com>
2022-02-23 11:23:51 +0800

    修改bug

--- platform-all-in-one-server/src/main/java/com/centit/platform/all/config/PlatformSpringMvcConfig.java
+++ platform-all-in-one-server/src/main/java/com/centit/platform/all/config/PlatformSpringMvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @author zou_wy
  * @date 2017/3/29
  */
-@ComponentScan(basePackages = {"com.centit.platform.controller"},
+@ComponentScan(basePackages = {"com.centit.platform.controller","com.centit.product.question.controller"},
         includeFilters = {@ComponentScan.Filter(value= org.springframework.stereotype.Controller.class)},
         useDefaultFilters = false)
 class PlatformSpringMvcConfig extends BaseSpringMvcConfig {
