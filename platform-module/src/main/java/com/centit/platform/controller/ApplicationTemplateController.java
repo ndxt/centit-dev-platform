844130e5ee9876d180523c6b0b3c6ead6ee7ae98
zhf <zhf@centit.com>
2023-02-28 11:39:28 +0800

    增加模板导入预处理接口

--- platform-module/src/main/java/com/centit/platform/controller/ApplicationTemplateController.java
+++ platform-module/src/main/java/com/centit/platform/controller/ApplicationTemplateController.java
@@ -146,6 +146,20 @@ public class ApplicationTemplateController extends BaseController {
         return modelExportManager.createApp(jsonObject, osId,
             WebOptUtils.getCurrentUserDetails(request));
     }
+    @ApiOperation(value = "根据模板导入应用返回预处理结果")
+    @RequestMapping(value = "/prepareTemplateApp", method = {RequestMethod.POST})
+    @WrapUpResponseBody
+    public JSONObject prepareTemplateApp(@RequestBody JSONObject jsonObject, HttpServletRequest request)  {
+        CentitUserDetails userDetails = WebOptUtils.getCurrentUserDetails(request);
+        if (userDetails==null){
+            throw new ObjectException(ResponseData.ERROR_USER_NOT_LOGIN, "您未登录，请先登录！");
+        }
+        if(jsonObject==null){
+            throw new ObjectException(ResponseData.ERROR_BAD_REQUEST,"导入内容没有填写");
+        }
+        return modelExportManager.prepareApp(jsonObject, "",
+            WebOptUtils.getCurrentUserDetails(request));
+    }
     @ApiOperation(value = "导入应用返回预处理结果")
     @RequestMapping(value = "/prepareUpdateApp/{osId}", method = {RequestMethod.POST})
     @WrapUpResponseBody
@@ -162,7 +176,7 @@ public class ApplicationTemplateController extends BaseController {
             WebOptUtils.getCurrentUserDetails(request));
     }
     @ApiOperation(value = "导入应用返回预处理结果")
-    @RequestMapping(value = "/importApp/{osId}", method = {RequestMethod.POST})
+    @RequestMapping(value = "/importApp", method = {RequestMethod.POST})
     @WrapUpResponseBody
     public Integer importApp(@RequestBody JSONObject jsonObject, HttpServletRequest request) throws Exception {
         CentitUserDetails userDetails = WebOptUtils.getCurrentUserDetails(request);
