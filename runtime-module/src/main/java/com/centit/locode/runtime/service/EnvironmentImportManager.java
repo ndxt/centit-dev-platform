fba7c6cecb298e0bf468b7e34c676dd27d6e8545
codefan <codefan@centit.com>
2023-07-14 11:36:35 +0800

    添加导入接口

--- runtime-module/src/main/java/com/centit/locode/runtime/service/EnvironmentImportManager.java
+++ runtime-module/src/main/java/com/centit/locode/runtime/service/EnvironmentImportManager.java
@@ -2,7 +2,17 @@ package com.centit.locode.runtime.service;
 
 import com.centit.framework.security.model.CentitUserDetails;
 
+/**
+ * 导入操作和导出操作，不是对称的，
+ * 导入操作仅仅导入文件信息和工作流引擎信息
+ * 作为一个额外的特性，可以导入数据字典
+ * @author codefan@sina.com
+ */
 public interface EnvironmentImportManager {
 
+    /**
+     * @param importType dictionary，file，flow
+     * @param ud 操作人员信息
+     */
     void importEnvironment(String importType, CentitUserDetails ud);
 }
--- runtime-module/src/main/java/com/centit/locode/runtime/service/impl/EnvironmentImportManagerImpl.java
+++ runtime-module/src/main/java/com/centit/locode/runtime/service/impl/EnvironmentImportManagerImpl.java
@@ -2,13 +2,42 @@ package com.centit.locode.runtime.service.impl;
 
 import com.centit.framework.security.model.CentitUserDetails;
 import com.centit.locode.runtime.service.EnvironmentImportManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import com.centit.fileserver.common.FileStore;
+
+import java.io.File;
 
 @Service
 public class EnvironmentImportManagerImpl implements EnvironmentImportManager {
 
+    @Value("${app.home:./}")
+    private String appHome;
+
+    @Autowired
+    protected FileStore fileStore;
+
+    private void importDictionary(String dictionaryDir) {
+
+    }
+
+    private void importFile(String fileDir) {
+
+    }
+
+    private void importWorkflow(String flowDir) {
+
+    }
+    /**
+     * @param importType dictionary，file，flow
+     * @param ud 操作人员信息
+     */
     @Override
     public void importEnvironment(String importType, CentitUserDetails ud) {
-
+        String rootDir = appHome + File.separator +  "config";
+        importDictionary(rootDir + File.separator + "dictionary");
+        importFile(rootDir + File.separator + "files");
+        importWorkflow(rootDir + File.separator + "flows");
     }
 }
