0adedcca980180d81f48ad98d8df7b42708b3bf8
codefan <codefan@centit.com>
2023-07-31 10:36:37 +0800

    修改默认的配置参数

--- platform-server/src/main/java/com/centit/locode/platform/config/InstantiationServiceBeanPostProcessor.java
+++ platform-server/src/main/java/com/centit/locode/platform/config/InstantiationServiceBeanPostProcessor.java
@@ -2,11 +2,11 @@ package com.centit.locode.platform.config;
 
 import com.centit.framework.components.CodeRepositoryCache;
 import com.centit.framework.components.OperationLogCenter;
-import com.centit.framework.core.controller.MvcConfigUtil;
 import com.centit.framework.model.adapter.MessageSender;
 import com.centit.framework.model.adapter.NotificationCenter;
 import com.centit.framework.model.adapter.OperationLogWriter;
 import com.centit.framework.model.adapter.PlatformEnvironment;
+import com.centit.support.json.JSONOpt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -40,7 +40,7 @@ public class InstantiationServiceBeanPostProcessor implements ApplicationListene
     @Override
     public void onApplicationEvent(@Nullable ContextRefreshedEvent event)
     {
-        MvcConfigUtil.fastjsonGlobalConfig();
+        JSONOpt.fastjsonGlobalConfig();
         CodeRepositoryCache.setPlatformEnvironment(platformEnvironment);
         if(innerMessageManager!=null) {
             notificationCenter.registerMessageSender("innerMsg", innerMessageManager);
