882a0a9a801588e40d4a2f95f29a90ed4dc2f141
codefan <codefan@centit.com>
2023-08-29 16:15:09 +0800

    修改bug

--- platform-all-in-one-server/src/main/java/com/centit/locode/platform/all/config/FrameworkSpringMvcConfig.java
+++ platform-all-in-one-server/src/main/java/com/centit/locode/platform/all/config/FrameworkSpringMvcConfig.java
@@ -3,7 +3,9 @@ package com.centit.locode.platform.all.config;
 import com.centit.framework.config.BaseSpringMvcConfig;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"com.centit.framework.system.controller", "com.centit.framework.tenant.controller"},
+@ComponentScan(basePackages = {"com.centit.framework.system.controller",
+                        "com.centit.framework.tenant.controller",
+                        "com.centit.framework.users.controller"},
     includeFilters = {@ComponentScan.Filter(value= org.springframework.stereotype.Controller.class)},
     useDefaultFilters = false)
 public class FrameworkSpringMvcConfig extends BaseSpringMvcConfig {
