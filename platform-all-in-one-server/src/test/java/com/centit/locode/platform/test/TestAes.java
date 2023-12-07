5a68a9b8566dd4be79594387b228cf3508cb7c9f
codefan <codefan@centit.com>
2023-12-07 15:41:22 +0800

    更改mysql驱动url

--- platform-all-in-one-server/src/test/java/com/centit/locode/platform/test/TestAes.java
+++ platform-all-in-one-server/src/test/java/com/centit/locode/platform/test/TestAes.java
@@ -8,6 +8,8 @@ public class TestAes  {
 
         System.out.println(SecurityOptUtils.decodeSecurityString(
             "aescbc:0zvjYuUGkJZuxz56T3jrGaa6pilpVxLocFor9DPuuy6/SIn3u31i9Vex76DPY0ymPDWRW/EnQwHYRY76706wzQ=="));
+        //cipher:KhktDzQz67BqoVkEOWgkEg==
+        System.out.println(SecurityOptUtils.decodeSecurityString("cipher:KhktDzQz67BqoVkEOWgkEg=="));
         //System.out.println(SecurityOptUtils.encodeSecurityString("cipher:KhktDzQz67BqoVkEOWgkEg==","cipher"));
     }
 }
