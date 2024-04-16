ef855e1d9a9332339070c44d43b4bec60a98062f
codefan <codefan@centit.com>
2024-04-16 15:42:16 +0800

    添加 全局版本 恢复逻辑接口

--- platform-module/src/main/java/com/centit/locode/platform/dao/ApplicationVersionDao.java
+++ platform-module/src/main/java/com/centit/locode/platform/dao/ApplicationVersionDao.java
@@ -1,10 +1,17 @@
 package com.centit.locode.platform.dao;
 
 import com.centit.framework.jdbc.dao.BaseDaoImpl;
+import com.centit.framework.jdbc.dao.DatabaseOptUtils;
 import com.centit.locode.platform.po.ApplicationVersion;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ApplicationVersionDao extends BaseDaoImpl<ApplicationVersion, String> {
 
+    public void setRestoreStatus(String appVersionId, String mergeStatus) {
+        DatabaseOptUtils.doExecuteSql(this,
+            "update application_version set MERGE_STATUS = ? " +
+                "where VERSION_ID = ? ",
+            new Object[] {mergeStatus, appVersionId});
+    }
 }
