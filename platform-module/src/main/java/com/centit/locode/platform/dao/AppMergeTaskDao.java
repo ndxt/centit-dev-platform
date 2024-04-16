05cea29c450bb816252dc2474b94ee8c91892a45
zfg <zfg@centit.com>
2024-04-16 10:23:58 +0800

    添加@Repository

--- platform-module/src/main/java/com/centit/locode/platform/dao/AppMergeTaskDao.java
+++ platform-module/src/main/java/com/centit/locode/platform/dao/AppMergeTaskDao.java
@@ -2,7 +2,9 @@ package com.centit.locode.platform.dao;
 
 import com.centit.framework.jdbc.dao.BaseDaoImpl;
 import com.centit.locode.platform.po.AppMergeTask;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class AppMergeTaskDao extends BaseDaoImpl<AppMergeTask, String> {
 
 }
