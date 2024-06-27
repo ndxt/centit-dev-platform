b20b6270284727e0ca3109684a0da2d5e7084af5
ndxt_yhs <codefan@centit.com>
2024-06-27 13:36:35 +0800

    merge source

--- platform-module/src/main/java/com/centit/locode/platform/service/impl/HistoryVersionServiceImpl.java
+++ platform-module/src/main/java/com/centit/locode/platform/service/impl/HistoryVersionServiceImpl.java
@@ -56,6 +56,8 @@ public class HistoryVersionServiceImpl implements HistoryVersionService {
 
     @Override
     public HistoryVersion getHistoryVersion(String historyId) {
+        if(historyId==null || historyId.isEmpty())
+            return null;
         return historyVersionDao.getObjectById(historyId);
     }
 
